393f514ce0123c81a311d6a70c571d8bea718f44
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-08-27 16:59:54 +0000

    [SCS-63] Criar SCS C#
    - Alterando as informacoes da DLL
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/trunk/src/csharp@109569 ae0415b3-e90b-0410-900d-d0be9363c56b

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("scs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SCS")]
+[assembly: AssemblyDescription("Sistema de Componente de Software")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PUC-Rio")]
-[assembly: AssemblyProduct("scs")]
+[assembly: AssemblyProduct("SCS")]
 [assembly: AssemblyCopyright("Copyright © PUC-Rio 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
