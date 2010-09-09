0772b415e561bb706968cb9b277c57b3aaeb53d3
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-09-09 14:11:49 +0000

    [SCS-63] Criar SCS C#
    - Atualizando os projetos.
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/trunk/src/csharp@109929 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/pingpong/component/Server/src/PingPongServant.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using scs.demos.pingpong;
+using Scs.Core;
+
+namespace Server
+{
+  class PingPongServant : MarshalByRefObject, PingPongServer
+  {
+    #region Fields
+
+    private ComponentContext context;
+
+    #endregion
+
+
+    #region Contructors
+
+    public PingPongServant(ComponentContext context) {
+      this.context = context;
+    }
+    
+    #endregion
+
+
+    #region PingPongServer Members
+
+    public void _stop() {
+      throw new NotImplementedException();
+    }
+
+    public int getId() {
+      throw new NotImplementedException();
+    }
+
+    public void ping() {
+      throw new NotImplementedException();
+    }
+
+    public void pong() {
+      throw new NotImplementedException();
+    }
+
+    public void setId(int identifier) {
+      throw new NotImplementedException();
+    }
+
+    public void start() {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
