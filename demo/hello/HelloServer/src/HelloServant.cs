5180374a506111e3e9d828c4f56c6ce56ea89d57
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2011-07-01 19:06:05 +0000

    [SCS-174] Criar o demo helloworld
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/csharp/trunk@119615 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/hello/HelloServer/src/HelloServant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using scs.demos.helloworld;
+using Scs.Core;
+
+namespace Server
+{
+  public class HelloServant : MarshalByRefObject, Hello
+  {
+    #region Fields
+
+    private ComponentContext context;
+
+    #endregion
+
+    #region Contructors
+
+    public HelloServant(ComponentContext context) {
+      this.context = context;
+    }
+
+    #endregion
+
+    #region Hello Members
+
+    public void sayHello() {
+      Console.WriteLine("Hello World!");
+    }
+
+    #endregion
+  }
+}
---
+++ demo/hello/HelloServer/src/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using System.Xml;
+using Ch.Elca.Iiop;
+using HelloServer.Properties;
+using omg.org.CORBA;
+using scs.core;
+using Scs.Core;
+using Scs.Core.Builder;
+
+namespace Server
+{
+  class Program
+  {
+    static void Main(string[] args) {
+
+      IiopChannel chan = new IiopChannel(0);
+      ChannelServices.RegisterChannel(chan, false);
+      
+      String componentModel = Resources.ComponentDesc;
+      TextReader file = new StringReader(componentModel);
+      XmlTextReader componentInformation = new XmlTextReader(file);
+      XMLComponentBuilder builder = new XMLComponentBuilder(componentInformation);
+      ComponentContext context = builder.build();
+
+      //Escrevendo a IOR do IComponent no arquivo.
+      IComponent component = context.GetIComponent();
+      OrbServices orb = OrbServices.GetSingleton();
+      String ior = orb.object_to_string(component);
+
+      String iorPath = Resources.IorFilename;
+      StreamWriter stream = new StreamWriter(iorPath);
+      try {
+        stream.Write(ior);
+      }
+      finally {
+        stream.Close();
+      }
+
+      Console.WriteLine("Componente Hello está no ar.");
+      Console.ReadLine();
+    }
+  }
+}
