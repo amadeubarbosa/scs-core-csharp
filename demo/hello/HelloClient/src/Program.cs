99c8714af85288069b600b0e09714af70efd5ae0
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2011-07-07 20:34:50 +0000

    [SCS-174] Criar o demo helloworld -- Faltou comitar a pasta src
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/csharp/trunk@119775 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/hello/HelloClient/src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using Ch.Elca.Iiop;
+using HelloClient.Properties;
+using omg.org.CORBA;
+using scs.core;
+using scs.demos.helloworld;
+
+namespace Client
+{
+  class Program
+  {
+    static void Main(string[] args) {
+
+      Console.WriteLine("Pressione 'enter' quando o componente Hello estiver no ar.");
+      Console.ReadLine();
+
+      IiopChannel chan = new IiopChannel();
+      ChannelServices.RegisterChannel(chan, false);
+
+      String helloIorPath = Resources.IorFilename;
+      StreamReader stream = new StreamReader(helloIorPath);
+      String helloIor;
+      try {
+        helloIor = stream.ReadToEnd();
+      }
+      finally {
+        stream.Close();
+      }
+            
+      OrbServices orb = OrbServices.GetSingleton();
+      IComponent icomponent = orb.string_to_object(helloIor) as IComponent;
+      Hello hello = icomponent.getFacetByName("Hello") as Hello;
+      hello.sayHello();
+                  
+      Console.ReadLine();
+    }
+  }
+}
